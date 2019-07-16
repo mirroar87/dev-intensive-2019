03a72cb1185c1aca4b8dd49d03a58f1cba07dbbb
mirroar87 <mirroar87@gmail.com>
2019-07-16 21:39:31 +0300

    hometask_3 восклицательный знак ! и пробел " "

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -24,7 +24,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
                 "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
             } else {
                 status = status.nextStatus()
-                "Это не правильный ответ\n${question.question}" to status.color
+                "Это неправильный ответ\n${question.question}" to status.color
             }
         }
         return "$string\n${question.question}" to status.color
